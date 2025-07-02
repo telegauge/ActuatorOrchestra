0228cce589d7ef85e2d054634e671d790130bb19
Mark Young <mainstreetmark@gmail.com>
2025-07-02 12:47:12 -0400

    api: home

--- lib/api/api.cpp
+++ lib/api/api.cpp
@@ -243,6 +243,12 @@ void init_api(WebServer &server, WebSocketsServer &ws, Ukulele *ukulele, OledDis
 		String response = handle_battery();
 		sendCORS(server, 200, "text/plain", response); });
 
+	server.on("/api/home", HTTP_GET, [&server]()
+						{
+		g_oled->log(API_PREFIX "home");
+		g_ukulele->home();
+		sendCORS(server, 200, "text/plain", "Homed"); });
+
 	server.on("/api/files", HTTP_GET, [&server]()
 						{
 		String out = "[";
